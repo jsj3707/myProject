2749c20ad1d416b4492add116e61a278e8801551
SeungJae Jung <jsj3707@naver.com>
2022-04-08 00:38:29 +0900

    firt commit

---
+++ html/main/16_radio,combo,checkbox/example_receive_single.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+    </head>
+    <body>
+        당신의 관심사는? <?=$_POST['interest']?>
+    </body>
+</html>
---
+++ html/main/17_fileupload/example_receive.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta http-equiv="content-type" content="text/html;charset=utf-8">
+    </head>
+    <body>
+        <?php
+        $uploaddir = '/var/www/codingeverybody_html/_tutorial/upload_75/';
+        $uploadfile= $uploaddir.basename[$_FILES['image']['name']];
+        
+         ?>
+         <img src="" />
+    </body>
+</html>
