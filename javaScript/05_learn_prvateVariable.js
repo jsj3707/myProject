96fd2d99ec40684dbaed100ac8fcf6f6341dbdc6
SeungJae Jung <jsj3707@naver.com>
2022-04-16 01:03:39 +0900

    소스 리네임 및 오늘분 공부 커밋.

---
+++ javaScript/05_learn_prvateVariable.js
@@ -0,0 +1,33 @@
+// 내부객체를 리턴하는 외부함수를 선언
+function factory_movie(title) {
+    return {
+        // 외부함수의 지역변수(매개변수)를 리턴함.
+        get_title : function(){
+            return title;
+        },
+        // 외부함수의 지역변수(매개변수)에 value 설정함.
+        set_title : function(_title){
+            if(typeof _title == 'String'){
+                title = _title
+            } else {
+                alert('제목은 문자열이어야 합니다.');
+            }
+        }
+    }
+}
+
+// 객체는 똑같지만 외부함수의 title변수에 담겨있는 값은 다름.
+ghost = factory_movie('Ghost in the shell');
+matrix = factory_movie('Matrix');
+
+// Ghost in the shell인 value를 공각기동대로 변경.
+ghost.set_title('공각기동대');
+
+alert(ghost.get_title()); // 공각기동대
+alert(matrix.get_title()); // Matrix
+
+/* 여기서 private variable은 외부함수의 지역변수인 title임.
+     외부함수의 수명은 끝난 시점에 title이란 지역변수는 get_title, set_title을 통해서만 접근가능함.
+     큰 규모의 소프트웨어의 경우 협업을 하기 때문에 title이란 변수를 누구나 접근하면 소프트웨어가 망가질 가능성이 큼.
+     즉, 클로저는 private variable을 사용할 수 있는 유용한 테크닉임.
+*/
---
+++ javaScript/06_learn_closure2.js
@@ -0,0 +1,41 @@
+
+// var arr = []
+// for(var i = 0; i < 5; i++){
+//     arr[i] = function(){
+                // 리턴하는 i는 함수의 외부 변수가 아니기때문.
+//         return i;
+//     }
+// }
+//
+// for(var index in arr){
+//     console.log(arr[index]);
+// }
+/* 결과
+5
+5
+5
+5
+5
+*/
+
+var arr = []
+for(var i = 0; i < 5; i++){
+// 매개변수id에는 i의 값이 들어감.
+    arr[i] = function (id) {
+        return function(){
+            return id;
+        }
+    }(i); // 외부함수의 인자값  = i
+}
+
+for(var index in arr){
+    console.log(arr[index]);
+}
+
+/* 결과
+0
+1
+2
+3
+4
+*/
